ce0f3bb89694ac78731397da033f221b0bc25b55
Mujeeburahman1124 <aqeelamrahman@gmail.com>
2026-02-07 21:37:36 +0530

    fix: restore missing Link import in DentistDashboard

--- src/pages/dentist/DentistDashboard.jsx
+++ src/pages/dentist/DentistDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { API_BASE_URL } from '../../config';
 import Navbar from '../../components/Navbar';
 
